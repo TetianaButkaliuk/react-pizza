ca1308c56b01298faa1de0e839573a95ea821e6d
Tetiana <butkaluk22@gmail.com>
2023-10-26 18:49:37 +0200

    add skeleton pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://6539052aa543859d1bb25ef6.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((item) => {
+              return <PizzaBlock {...item} key={item.id} />;
+            })}
+      </div>
+    </>
+  );
+};
+
+export default Home;
