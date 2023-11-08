2f4cbf3c67b035b2318987a8524a4720ab0ae756
Tetiana <butkaluk22@gmail.com>
2023-11-08 15:37:15 +0100

    pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,9 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
+import Pagination from "../components/Pagination/Pagination";
 
-const Home = (props) => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
@@ -14,14 +15,16 @@ const Home = (props) => {
     name: "популярности",
     sortProperty: "rating",
   });
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const category = categoryId > 0 ? `category=${categoryId}` : "";
     const sortBy = sortType.sortProperty.replace("-", "");
     const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const search = searchValue ? `&search=${searchValue}` : "";
 
     fetch(
-      `https://6539052aa543859d1bb25ef6.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://6539052aa543859d1bb25ef6.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => {
         return res.json();
@@ -31,15 +34,15 @@ const Home = (props) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   const pizzas = items
-    .filter((item) => {
-      if (item.title.toLowerCase().includes(props.searchValue.toLowerCase())) {
-        return true;
-      }
-      return false;
-    })
+    // .filter((item) => {
+    //   if (item.title.toLowerCase().includes(searchValue.toLowerCase())) {
+    //     return true;
+    //   }
+    //   return false;
+    // })
     .map((item) => {
       return <PizzaBlock {...item} key={item.id} />;
     });
@@ -56,6 +59,7 @@ const Home = (props) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
